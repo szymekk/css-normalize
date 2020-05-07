db4d4dd61b8898df65aa7cd90589cd7e7dc809b8
Szymon Kozłowski <szymek.kozlowski1@gmail.com>
2020-05-07 16:49:53 +0200

    Add tests with Text inputs
    Add tests which feed parsers with token lists obtained indirectly by
    tokenizing Text inputs.

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import Data.CSS.Syntax.Tokens
+import Data.List
 import Parse
 import Test.Hspec
 import Test.Hspec.Megaparsec
@@ -11,9 +12,8 @@ main = hspec spec
 
 spec :: Spec
 spec =
-  describe "Parse"
-    $ describe "parse tokens"
-    $ do
+  describe "Parse" $ do
+    describe "parse tokens" $ do
       it "parses property values" $ do
         testParse parseDeclarationValues [i "a", Semicolon]
           `shouldParse` [i "a"]
@@ -35,9 +35,57 @@ spec =
           `shouldParse` QualifiedRule [i "body"] []
         testParse parseQualifiedRule [i "body", curlyL, i "key", Colon, i "v", curlyR]
           `shouldParse` QualifiedRule [i "body"] [Declaration (Key "key") [i "v"]]
+    describe "parse from text" $ do
+      it "parses declaration" $ do
+        let one = Number "1" (NVInteger 1)
+            function = [Function "rgb", one, Comma, one, Comma, one, RightParen]
+            parsedDeclaration = Declaration (Key "color") function
+            parsedDeclaration' = Declaration (Key "color") (intersperse ws function)
+        parseText parseOneDeclaration "color:rgb(1,1,1);" `shouldParse` parsedDeclaration
+        parseText parseOneDeclaration "color : rgb( 1 , 1 , 1 ) ;" `shouldParse` parsedDeclaration'
+      it "parses qualified rule" $ do
+        testParse parseQualifiedRule `shouldFailOnText` ""
+        testParse parseQualifiedRule `shouldFailOnText` " "
+        parseText parseQualifiedRule "body{}" `shouldParse` QualifiedRule [i "body"] []
+        parseText parseQualifiedRule "body{/*test*/}" `shouldParse` QualifiedRule [i "body"] []
+        parseText parseQualifiedRule "*{k:v}" `shouldParse` QualifiedRule [d '*'] [Declaration (Key "k") [i "v"]]
+        let parsedRule =
+              QualifiedRule
+                [i "a", ws, i "b"]
+                [ Declaration
+                    (Key "margin")
+                    [ Dimension "30" (NVInteger 30) "px",
+                      ws,
+                      Number "0" (NVInteger 0)
+                    ]
+                ]
+        parseText parseQualifiedRule "a b{margin:30px 0}" `shouldParse` parsedRule
+        parseText parseQualifiedRule "a b{margin:30px 0;}" `shouldParse` parsedRule
+        parseText parseQualifiedRule "a b { margin : 30px 0 ; } " `shouldParse` parsedRule
+        parseText parseQualifiedRule "a b  {  margin  :  30px 0  ;  } " `shouldParse` parsedRule
+        parseText parseQualifiedRule ".a {}"
+          `shouldParse` QualifiedRule [d '.', i "a"] []
+        parseText parseQualifiedRule "a > b{}"
+          `shouldParse` QualifiedRule [i "a", ws, d '>', ws, i "b"] []
+        -- insert comments to prevent collapsing whitespace
+        parseText parseQualifiedRule "a /**/ > /**/ b /**/ { /**/ } "
+          `shouldParse` QualifiedRule [i "a", ws, ws, d '>', ws, ws, i "b"] []
+        let parsedRule2 =
+              QualifiedRule
+                [i "a"]
+                [ Declaration (Key "k1") [i "v1"],
+                  Declaration (Key "k2") [i "v2"]
+                ]
+        parseText parseQualifiedRule "a{k1:v1;k2:v2} " `shouldParse` parsedRule2
+        parseText parseQualifiedRule "a{k1:v1;k2:v2;} " `shouldParse` parsedRule2
+        parseText parseQualifiedRule "a {k1 : v1 ; k2 : v2 } " `shouldParse` parsedRule2
+        parseText parseQualifiedRule "a {k1 : v1 ; k2 : v2 ; } " `shouldParse` parsedRule2
   where
     testParse p = parse p ""
+    shouldFailOnText x text = shouldFailOn x (tokenize text)
+    parseText p text = parse p "" (tokenize text)
     ws = Whitespace
     i = Ident
+    d = Delim
     curlyL = LeftCurlyBracket
     curlyR = RightCurlyBracket
