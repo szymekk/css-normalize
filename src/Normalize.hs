dbecc91b47f21121156ae54dccb735f0b1285574
Szymon Kozłowski <szymek.kozlowski1@gmail.com>
2020-05-22 01:59:24 +0200

    Normalize numeric values with zero integer part

--- src/Normalize.hs
+++ src/Normalize.hs
@@ -1,6 +1,12 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Normalize where
 
+import Balanced
+import Balanced.Internal
+import Data.CSS.Syntax.Tokens as CSS
 import Data.List
+import Data.Text
 import Types
 
 -- | Transform a stylesheet into another one in a canonical form.
@@ -21,8 +27,36 @@ normalizeStyleRule (QualifiedRule prelude declarations) =
   QualifiedRule prelude' declarations'
   where
     prelude' = normalizeSelectorsGroup prelude
-    declarations' = sortBy compareKeys declarations
+    declarations' =
+      sortBy compareKeys (fmap normalizeOneDeclaration declarations)
     compareKeys (Declaration k1 _) (Declaration k2 _) = k1 `compare` k2
+    normalizeOneDeclaration (Declaration key values) =
+      let values' = UnsafeBalanced $ fmap normalizeToken (unBalanced values)
+       in Declaration key values'
+
+normalizeToken :: CSS.Token -> CSS.Token
+normalizeToken = mapNumericToken addLeadingZero
+
+mapNumericToken :: (Text -> Text) -> CSS.Token -> CSS.Token
+mapNumericToken f (Number t v) = Number (f t) v
+mapNumericToken f (Dimension t nv u) = Dimension (f t) nv u
+mapNumericToken f (Percentage t nv) = Percentage (f t) nv
+mapNumericToken _ x = x
+
+-- | Add leading zero to strings representing numeric literals with an ommited
+-- integer part i.e those starting with a decimal comma.
+--
+-- >>> addLeadingZero "-.5"
+-- "-0.5"
+addLeadingZero :: Text -> Text
+addLeadingZero text = case Data.Text.uncons text of
+  Just (x, xs)
+    | x == '.' -> "0" <> text
+    | x `elem` ['+', '-'] -> case Data.Text.uncons xs of
+      Just (x', _xs') -> if x' == '.' then singleton x <> "0" <> xs else text
+      Nothing -> text
+    | otherwise -> text
+  Nothing -> text
 
 -- | Normalize a group of selectors by sorting its' constituent selectors.
 -- The order in which selectors appear in a comma separated group of selectors
--- test/NormalizeSpec.hs
+++ test/NormalizeSpec.hs
@@ -28,6 +28,40 @@ spec =
         normalizeSelectorsGroup `isIdempotentOn` input
         normalizeSelectorsGroup `isIdempotentOn` sorted
         normalizeSelectorsGroup `isIdempotentOn` []
+    describe "normalize tokens" $ do
+      it "does not modify strings containing numeric values" $ do
+        normalizeToken <$> t "\'.2\'" `shouldBe` [String ".2"]
+        normalizeToken <$> t "\".2\"" `shouldBe` [String ".2"]
+      it "does not modify normalized numeric values" $ do
+        normalizeToken <$> t "0.1 +0.2 -0.3" `shouldBe` t "0.1 +0.2 -0.3"
+        normalizeToken <$> t "1.1 +1.2 -1.3" `shouldBe` t "1.1 +1.2 -1.3"
+      it "adds leading zero in numeric values" $ do
+        normalizeToken <$> t ".1 +.2 -.3" `shouldBe` t "0.1 +0.2 -0.3"
+        normalizeToken <$> t ".1% +.2% -.3%" `shouldBe` t "0.1% +0.2% -0.3%"
+        normalizeToken <$> t ".1px +.2px -.3px" `shouldBe` t "0.1px +0.2px -0.3px"
+        normalizeToken <$> t ".1" `shouldBe` [Number "0.1" (NVNumber 0.1)]
+        normalizeToken <$> t "+.1" `shouldBe` [Number "+0.1" (NVNumber 0.1)]
+        normalizeToken <$> t "-.1" `shouldBe` [Number "-0.1" (NVNumber (-0.1))]
+        normalizeToken <$> t ".2%" `shouldBe` [Percentage "0.2" (NVNumber 0.2)]
+        normalizeToken <$> t "+.2%" `shouldBe` [Percentage "+0.2" (NVNumber 0.2)]
+        normalizeToken <$> t "-.2%" `shouldBe` [Percentage "-0.2" (NVNumber (-0.2))]
+        normalizeToken <$> t ".3em" `shouldBe` [Dimension "0.3" (NVNumber 0.3) "em"]
+        normalizeToken <$> t "+.3em" `shouldBe` [Dimension "+0.3" (NVNumber 0.3) "em"]
+        normalizeToken <$> t "-.3em" `shouldBe` [Dimension "-0.3" (NVNumber (-0.3)) "em"]
+      it "is idempotent" $ do
+        normalizeToken `isIdempotentOn` Semicolon
+        normalizeToken `isIdempotentOn` Colon
+        normalizeToken `isIdempotentOn` Delim '!'
+        normalizeToken `isIdempotentOn` LeftCurlyBracket
+        fmap normalizeToken `isIdempotentOn` t "+.3em"
+        fmap normalizeToken `isIdempotentOn` t "+0.3em"
+        fmap normalizeToken `isIdempotentOn` t "+1.3em"
+        fmap normalizeToken `isIdempotentOn` t "-.3em"
+        fmap normalizeToken `isIdempotentOn` t "-0.3em"
+        fmap normalizeToken `isIdempotentOn` t "-1.3em"
+        fmap normalizeToken `isIdempotentOn` t ".3em"
+        fmap normalizeToken `isIdempotentOn` t "0.3em"
+        fmap normalizeToken `isIdempotentOn` t "1.3em"
   where
     ws = Whitespace
     mkEmptyDecl k = Declaration (Key k) mempty
